cb00083a573a54175cd22967819290446806a789
AidanDavis1802 <davisa2931@student.faytechcc.edu>
2026-09-17 14:48:13 -0400

    Calculating integers
    Calculating integers with exponents and addition and subtraction using Python.

---
+++ P1H1_DavisAidan.py
@@ -0,0 +1,27 @@
+# Aidan Davis
+#09/17/2026
+# P1H1.py
+# A simple calculating program
+
+#Calculating Exponents
+
+print("----------Calculating Exponents----------")
+print()
+
+base = int(input("Enter a base number: "))
+exponent = int(input("Enter an exponent: "))
+result = base ** exponent
+print(base, "raised to the power of", exponent, "is", result, "!!")
+
+
+#Simple addition and subtraction
+
+print("----------Addition and Subtraction----------")
+print()
+
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to subtract: "))
+num3 = int(input("Enter an integer to subtract: "))
+result = num1 + num2 - num3
+print(num1, "+", num2, "-", num3, "is equal to", result)
+
