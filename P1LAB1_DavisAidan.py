be226b47d63870ed2af23a9cee92e16667e6692e
AidanDavis1802 <davisa2931@student.faytechcc.edu>
2026-09-16 21:41:54 -0400

    First Python Assignment
    Taking input from user and printing output

---
+++ P1LAB1_DavisAidan.py
@@ -0,0 +1,14 @@
+# Aidan Davis
+# 09/16/2026
+# P1Lab1.py
+# A basic program that does input and output printing
+
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+#I know this assignment statement is not required in the assignment, I just wanted to add it to understand python slightly better
+class_name = "CTI-110"
+
+print ("Hello,", first_name,last_name + "!", "Welcome to", class_name + "!")
